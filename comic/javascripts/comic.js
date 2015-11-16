f3a0cc510f4b125df77daee6160b626bb86aecb7
Jamela Black <jamela.black@gmail.com>
2015-11-16 10:14:52 -0500

    comic

---
+++ comic/javascripts/comic.js
@@ -0,0 +1,25 @@
+$(function() {
+  var $blinds = $("[id^=blind]");
+  var delay = 1500,
+      speed = 750;
+
+  function startAnimation() {
+    $blinds.each(function(i) {
+      var $blind = $blinds.eq(i);
+      $blind.delay(delay + i * speed ).animate({
+        top: "+= " +$blind.height(),
+        height: 0
+      }, speed);
+    });
+  }
+
+  $("a").on("click",function(e) {
+    e.preventDefault();
+
+    $blinds.removeAttr("style");
+    startAnimation();
+  });
+
+  startAnimation();
+});
+
