1122a2c994bbfe6d515f7369cedf4bde6e68821a
Jamela Black <jamela.black@gmail.com>
2016-02-16 21:03:03 -0500

    Model Constructor Seeding and Change Events

---
+++ framework-2/javascripts/framework.js
@@ -0,0 +1,42 @@
+function ModelConstructor(options) {
+  var id_count = 0;
+
+  function Model(attrs) {
+    id_count++;
+
+    var self = this;
+    self.attributes = attrs || {};
+    self.id = id_count;
+    self.attributes.id = id_count;
+
+    if (options && options.change && _.isFunction(options.change)) {
+      this.__events.push(options.change);
+    }
+  }
+
+  Model.prototype = {
+    __events: [],
+    set: function(key, val) {
+      this.attributes[key] = val;
+      this.triggerChange();
+    },
+    get: function(key) {
+      return this.attributes[key];
+    },
+    remove: function(key) {
+      return this.attributes[key];
+    },
+    triggerChange: function() {
+      this.__events.forEach(function(cb) {
+       cb();
+      });
+    },
+    addCallback: function(cb) {
+      this.__events.push(cb);
+    }
+  };
+
+  _.extend(Model.prototype, options);
+
+  return Model;
+}
